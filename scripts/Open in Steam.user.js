1decfdee4f806f43001e8a889df6487381ed5d85
ooa113y <13thSlayer@gmail.com>
2021-09-20 16:13:34 +0300

    Update Open in Steam.user.js

--- scripts/Open in Steam.user.js
+++ scripts/Open in Steam.user.js
@@ -5,14 +5,14 @@
 // @match       https://store.steampowered.com/*
 // @match       https://steamcommunity.com/*
 // @match       https://help.steampowered.com/*
-// @grant       GM_registerMenuCommand
+// @grant       none
 // ==/UserScript==
 
 const original = document.querySelector('.header_installsteam_btn_content')
 const button = original.cloneNode()
 button.style.backgroundImage = 'none'
 button.style.paddingLeft = '9px'
-button.style.backgroundColor = 'green'
+button.style.background = 'linear-gradient(60deg, rgba(117, 176, 34, 0.7) 5%,rgba(88, 138, 27, 0.6) 95%)'
 button.style.marginRight = '9px'
 button.href = `steam://openurl/${location.href}`
 button.innerText = 'Open in Steam'
