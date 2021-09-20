04f000111e6fec88b5817f491b34da078c6849b5
ooa113y <13thSlayer@gmail.com>
2021-09-20 17:45:41 +0300

    Deal with non-FLAC albums

--- scripts/KHInsider mass download.user.js
+++ scripts/KHInsider mass download.user.js
@@ -4,13 +4,15 @@
 // @match        https://downloads.khinsider.com/*
 // @description  Allows mass downloads of soundtracks from downloads.khinsider.com.
 // @homepageURL  https://github.com/ooa113y/userscripts/tree/master/scripts
-// @version      1
+// @version      3
 // @grant GM_xmlhttpRequest
 // @grant GM_download
 // @grant GM_getValue
 // @grant GM_setValue
 // @connect vgmsite.com
 // ==/UserScript==
+
+GM_setValue('format', GM_getValue('format', 'mp3'))
 
 let idx = 1
 const songs = Array.from(document.getElementsByClassName('playlistDownloadSong')).map(x => {
@@ -23,12 +25,22 @@ const songs = Array.from(document.getElementsByClassName('playlistDownloadSong')
 const padLength = String(document.getElementById('songlist').rows.length - 2).length
 const dlPage = document.createElement('div')
 
+let alerted = false
+
 function download (suffix) {
     for (const song of songs) {
         GM_xmlhttpRequest({url: song.url, onload: data => {
             dlPage.innerHTML = data.responseText
+            const link = dlPage.querySelector(`a[href$=${suffix}]`)
+            if (!link) {
+              if (!alerted) alert(`${GM_getValue('format').toUpperCase()} is not supported for this album. Switching to MP3.`)
+              alerted = true
+              GM_setValue('format', 'mp3')
+              location.reload()
+              return
+            }
             GM_download(
-              dlPage.querySelector(`a[href$=${suffix}]`).href,
+              link.href,
               `${String(song.idx).padStart(padLength, '0')} - ${song.name}.${suffix}`
             )
         }})
@@ -37,18 +49,18 @@ function download (suffix) {
 
 const dlink = document.querySelector('.albumMassDownload div a')
 dlink.href = '#'
-dlink.innerHTML = `click to download as ${GM_getValue('format', 'mp3').toUpperCase()}`
+dlink.innerHTML = `click to download as ${GM_getValue('format').toUpperCase()}`
 dlink.addEventListener('click', event => {
   event.preventDefault()
   dlink.innerText = 'Downloading, please wait...'
-  download(GM_getValue('format', 'mp3'))
+  download(GM_getValue('format'))
 })
 
 const clink = document.createElement('a')
 clink.innerText = ' (change format?)'
 clink.href = '#'
 clink.addEventListener('click', _ => {
-  GM_setValue('format', GM_getValue('format', 'mp3') === 'mp3' ? 'flac' : 'mp3')
+  GM_setValue('format', GM_getValue('format') === 'mp3' ? 'flac' : 'mp3')
   location.reload()
 })
 dlink.parentNode.appendChild(clink)
