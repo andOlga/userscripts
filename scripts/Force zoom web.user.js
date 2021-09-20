1628090d8887b2e45e6b4fe4d69401b3b293b236
ooa113y <13thSlayer@gmail.com>
2021-09-20 13:43:15 +0300

    Update Force zoom web.user.js

--- scripts/Force zoom web.user.js
+++ scripts/Force zoom web.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Force zoom web
+// @name        Force Zoom web
 // @namespace   https://github.com/ooa113y/userscripts
 // @description Forces Zoom links to open in a web browser, so that it doesn't try to force the client download on you.
 // @match       https://*.zoom.us/s/*
