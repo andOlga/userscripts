26e1261a04e83dc3e162cd372f6faecb41666a56
ooa113y <13thSlayer@gmail.com>
2021-09-20 15:23:58 +0300

    Update GitHub editor button.user.js

--- scripts/GitHub editor button.user.js
+++ scripts/GitHub editor button.user.js
@@ -4,14 +4,16 @@
 // @namespace   https://github.com/ooa113y/userscripts
 // @match       https://github.com/*
 // @grant       none
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@1
 // ==/UserScript==
 
-setTimeout(_ => { // The buttons don't show up immediately on page load, so wait a bit for them to render
+VM.observe(document.body, _ => {
   let findButton = document.querySelector('[data-hydro-click*=FIND_FILE_BUTTON]')
   if (findButton) {
     let editButton = findButton.cloneNode()
     editButton.href = `https://github.dev/${location.pathname.replace('/', '')}`
     editButton.innerText = 'Open in web editor'
     findButton.parentNode.insertBefore(editButton, findButton)
+    return true
   }
-}, 1000)
+})
