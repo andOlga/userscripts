727d7f645935519c327fe87fc88fbbc49f7aff77
ooa113y <13thSlayer@gmail.com>
2021-09-21 14:04:28 +0300

    Create Nitter image download.user.js

---
+++ scripts/Nitter image download.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Nitter image download
+// @namespace   https://github.com/ooa113y/userscripts
+// @homepageURL https://github.com/ooa113y/userscripts/tree/master/scripts
+// @description Download images from Nitter with sensible filenames
+// @version     1
+// @match       https://nitter.net/pic/*
+// @grant       GM_download
+// ==/UserScript==
+
+const button = document.createElement('button')
+button.innerText = '⬇'
+button.style.position = 'absolute'
+button.style.left = 0
+button.style.top = 0
+button.addEventListener('click', _ => {
+  GM_download(location.pathname, location.pathname.replace('/pic/media%2F', '').replace('%3Fname%3Dorig', ''))
+})
+document.body.appendChild(button)
