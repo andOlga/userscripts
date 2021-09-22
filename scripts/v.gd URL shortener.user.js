1974569b973af1c69cbbd722e1126c01a248ea48
ooa113y <13thSlayer@gmail.com>
2021-09-22 19:04:36 +0300

    Update v.gd URL shortener.user.js

--- scripts/v.gd URL shortener.user.js
+++ scripts/v.gd URL shortener.user.js
@@ -2,13 +2,26 @@
 // @name        v.gd URL shortener
 // @namespace   https://github.com/ooa113y/userscripts
 // @homepageURL https://github.com/ooa113y/userscripts/tree/master/scripts
+// @icon        https://v.gd/favicon.ico
 // @description Shortens URLs with the v.gd service
-// @version     1
+// @version     2
 // @match       *://*/*
 // @grant       GM_setClipboard
 // @grant       GM_registerMenuCommand
+// @grant       GM_xmlhttpRequest
+// @grant       GM_notification
+// @connect     v.gd
 // ==/UserScript==
 
 GM_registerMenuCommand('Shorten', _ => {
   window.open('https://v.gd/create.php?url=' + encodeURIComponent(location.href))
+})
+
+GM_registerMenuCommand('Shorten to clipboard', _ => {
+  GM_xmlhttpRequest({
+    url: 'https://v.gd/create.php?format=simple&url=' + encodeURIComponent(location.href),
+    onload: data => {
+      GM_setClipboard(data.responseText)
+      GM_notification('Short URL copied!')
+    }})
 })
