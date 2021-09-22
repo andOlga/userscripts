80c56029ad85b671d39ac1be1897a82db6415a64
ooa113y <13thSlayer@gmail.com>
2021-09-22 07:59:03 +0300

    Update Twitter to Nitter.user.js

--- scripts/Twitter to Nitter.user.js
+++ scripts/Twitter to Nitter.user.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/ooa113y/userscripts
 // @homepageURL https://github.com/ooa113y/userscripts/tree/master/scripts
 // @description Redirects any Twitter links to Nitter, to get rid of annoying log-in pop-ups and other UI hindrances
-// @version     1
+// @version     2
 // @match       https://*.twitter.com/*
 // @grant       GM_getValue
 // @run-at      document-start
 // ==/UserScript==
 
-location.href = location.href.replace('twitter.com', GM_getValue('instance', 'nitter.net'))
+location.hostname = GM_getValue('instance', 'nitter.net')
