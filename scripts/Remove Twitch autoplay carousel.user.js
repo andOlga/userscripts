a071f5e0f19602873d682ac95166fb0086b159e2
ooa113y <13thSlayer@gmail.com>
2021-10-07 18:49:02 +0300

    Create Remove Twitch autoplay carousel.user.js

---
+++ scripts/Remove Twitch autoplay carousel.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Remove Twitch autoplay carousel
+// @namespace   https://github.com/ooa113y/userscripts
+// @homepageURL https://github.com/ooa113y/userscripts/tree/master/scripts
+// @version     1
+// @match       https://www.twitch.tv/
+// @grant       none
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@1
+// @icon        https://www.twitch.tv/favicon.ico
+// ==/UserScript==
+
+VM.observe(document.body, _ => {
+  let car
+  if (car = document.querySelector('.front-page-carousel')) {
+    car.remove()
+    return true
+  }
+})
