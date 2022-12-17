1b74667785c37e0bafd3b25d5d8fb432cbe7c11f
&Olga <462484+andOlga@users.noreply.github.com>
2022-12-17 17:53:21 +0400

    Replace Twitch's "popout player" with PIP option

--- scripts/Twitch PIP.user.js
+++ scripts/Twitch PIP.user.js
@@ -2,12 +2,23 @@
 // @name        Twitch PIP
 // @namespace   https://github.com/andOlga/userscripts
 // @homepageURL https://github.com/andOlga/userscripts/tree/master/scripts
-// @description Allows to open the Twitch player in PIP
+// @description Replace the Twitch "Popout Player" with standard Picture in Picture
 // @icon        https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png
-// @version     1
+// @version     6
 // @match       https://www.twitch.tv/*
-// @grant       GM_registerMenuCommand
 // ==/UserScript==
-GM_registerMenuCommand('Open stream in PIP', _ => {
-  document.getElementsByTagName('video')[0].requestPictureInPicture()
+const mo = new MutationObserver(_ => {
+  const btn = [...document.getElementsByTagName('button')].find(x => x.innerText === 'Popout Player')
+  if (btn) {
+    btn.children[0].children[0].innerText = 'Picture in Picture'
+    btn.addEventListener('click', event => {
+      event.preventDefault()
+      event.stopImmediatePropagation()
+      event.stopPropagation()
+      document.getElementsByTagName('video')[0].requestPictureInPicture()
+      const closeBtn = [...document.getElementsByTagName('button')].find(x => x.innerText === "Close")
+      closeBtn.click()
+    })
+  }
 })
+mo.observe(document.body, { subtree: true, childList: true })
