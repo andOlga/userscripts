75e04770b4edfc404cc0a9a223eb1aec7cb15bec
&Olga <462484+andOlga@users.noreply.github.com>
2023-05-20 16:35:39 +0400

    Create idgames download improvements.user.js

---
+++ scripts/idgames download improvements.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        /idgames download improvements
+// @namespace   https://github.com/andOlga/userscripts
+// @homepageURL https://github.com/andOlga/userscripts/tree/master/scripts
+// @description Adjusts the download section of /idgames pages to be more user-friendly
+// @icon        https://www.idsoftware.com/favicon.ico
+// @version     1
+// @match       https://www.doomworld.com/idgames/*
+// @grant       GM_download
+// ==/UserScript==
+const downloadTable = document.querySelector('table.download')
+if (downloadTable) {
+  const links = [...downloadTable.querySelectorAll('a')]
+  for (const link of links) {
+    if (link.href.startsWith('ftp://')) {
+      // Get rid of FTP links altogether -- most browsers no longer support these...
+      link.parentNode.remove()
+    } else {
+      // For remaining links, download them as PK3 instead of ZIP, so that they're instantly playable
+      const fileElements = link.href.split('/')
+      const fileName = fileElements[fileElements.length - 1].replace(/\.zip$/, '.pk3')
+      link.onclick = event => {
+        event.preventDefault()
+        GM_download({url: link.href, name: fileName})
+      }
+    }
+  }
+}
